46f593d6ea8c92a6ae49376592b4e0dbfc9408b7
cissy <1787393382@qq.com>
2025-09-04 21:38:19 +0800

    An in-memory reliable byte stream

--- src/byte_stream.cc
+++ src/byte_stream.cc
@@ -2,55 +2,61 @@
 
 using namespace std;
 
-ByteStream::ByteStream( uint64_t capacity ) : capacity_( capacity ) {}
+ByteStream::ByteStream( uint64_t capacity )
+  : buffer_(), capacity_( capacity ), bytes_pushed_( 0 ), bytes_popped_( 0 ), is_close_( false ), error_( false )
+{}
 
 void Writer::push( string data )
 {
-  (void)data; // Your code here.
+  uint64_t write_len = min( data.size(), available_capacity() );
+  Writer::buffer_ += data.substr( 0, write_len );
+  bytes_pushed_ += write_len;
 }
 
 void Writer::close()
 {
-  // Your code here.
+  is_close_ = true;
 }
 
 bool Writer::is_closed() const
 {
-  return {}; // Your code here.
+  return is_close_;
 }
 
 uint64_t Writer::available_capacity() const
 {
-  return {}; // Your code here.
+  return capacity_ - buffer_.size();
 }
 
 uint64_t Writer::bytes_pushed() const
 {
-  return {}; // Your code here.
+  return bytes_pushed_;
 }
 
 string_view Reader::peek() const
 {
-  return {}; // Your code here.
+  std::string_view sv1( buffer_ );
+  return { sv1 };
 }
 
 void Reader::pop( uint64_t len )
 {
-  (void)len; // Your code here.
+  uint64_t pop_len = min( len, bytes_buffered() );
+  Reader::buffer_.erase( 0, pop_len );
+  bytes_popped_ += pop_len;
 }
 
 bool Reader::is_finished() const
 {
-  return {}; // Your code here.
+  return is_close_ && buffer_.empty();
 }
 
 uint64_t Reader::bytes_buffered() const
 {
-  return {}; // Your code here.
+  return buffer_.size();
 }
 
 uint64_t Reader::bytes_popped() const
 {
-  return {}; // Your code here.
+  return bytes_popped_;
 }
-
